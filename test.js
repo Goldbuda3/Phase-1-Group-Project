7f628d59bc0b988b766a9c1530ed783cafa6bfab
John Melegrito <john_melegrito@yahoo.com>
2019-11-09 13:42:32 -0600

    fixed style and added render

--- test.js
+++ test.js
@@ -16,7 +16,7 @@
 //     headers: {
 //         "user-key": "5a22b8c4073d2f0d885f503903f53635",
 //     },
-    
+
 //     data:
 //         "fields name; where platforms = 48;exclude tags;"
 //     ,
@@ -51,56 +51,70 @@ let doodad = "ef77360c75de1c722453c99cebf0f44843f09d27";
 //Switch ID = 157
 
 //Grabbing stuff based on platform and MM/YYYY
-// $.ajax({
-//     type: 'GET',
-//     dataType: 'jsonp',
-//     crossDomain: true,
-//     jsonp: 'json_callback',
-//     url: `http://www.giantbomb.com/api/games/?format=jsonp&api_key=${doodad}&filter=platforms:94,expected_release_year:2019,expected_release_month:10`,
-//     complete: function () {
-//         console.log('done');
-//     },
-//     success: function (data) {
-//         console.log(data);
-//         console.log(data.results)
-//         let arr = data.results
-//         function renderGames(array){
-//             let gameHTML = array.map(function(thing){
-//                 let gameName = `<div>${thing.name}</div>
-//                 <div>${thing.deck}</div>
-//                 <div><img src="${thing.image.small_url}"></div>`
-//                 return gameName
-//             }).join("")
-//             return gameHTML
-//         }
-        
-//         function renderStuff(){
-//             let content = $("#test").html(renderGames(arr))
-//         }
-        
-//         renderStuff();
-        
+$.ajax({
+    type: 'GET',
+    dataType: 'jsonp',
+    crossDomain: true,
+    jsonp: 'json_callback',
+    url: `http://www.giantbomb.com/api/games/?format=jsonp&api_key=${doodad}&filter=platforms:94,expected_release_year:2019,expected_release_month:10`,
+    complete: function () {
+        console.log('done');
+    },
+    success: function (data) {
+        console.log(data);
+        console.log(data.results)
+        let arr = data.results
+        function renderGames(array) {
+            let gameHTML = array.map(function (thing) {
+                let gameName = `
+                
+                <div class="flip-card">
+<div class="flip-card-inner">
+    <div class="flip-card-front">
+    <div class="image-container">
+        <img src="${thing.image.super_url}" alt="Avatar"">
+        </div>
+    </div>
+    <div class="flip-card-back">
+        <h1>${thing.name}</h1>
+        <p>${thing.deck}</p>
+    </div>
+</div>
+</div>
+</div>
+                `
+                return gameName
+            }).join("")
+            return gameHTML
+        }
 
-//     }
-// });
 
-$.get("https://api.bestbuy.com/v1/products((search=Indivisible)&(categoryPath.id=pcmcat295700050012))?apiKey=7OKoNjvIWrA9haGpaqBtgXAl&pageSize=1&format=json").then(function(response){
+        $(".container-card").html(renderGames(arr))
+
+
+
+    }
+});
+
+
+
+$.get("https://api.bestbuy.com/v1/products((search=Indivisible)&(categoryPath.id=pcmcat295700050012))?apiKey=7OKoNjvIWrA9haGpaqBtgXAl&pageSize=1&format=json").then(function (response) {
     console.log(response);
 
     let arr = response
-        function renderGames(response){
-                let gameName = `<div><a href="${response.products[0].addToCartUrl}">${response.products[0].name}</a></div>
+    function renderGames(response) {
+        let gameName = `<div><a href="${response.products[0].addToCartUrl}">${response.products[0].name}</a></div>
                 
                 <div><img src="${response.products[0].image}"></div>
                 
                 <div>$${response.products[0].regularPrice}</div>`
-                return gameName
-        }
-        
-        function renderStuff(){
-            let content = $("#test").html(renderGames(arr))
-        }
-        
-        renderStuff();
+        return gameName
+    }
+
+    function renderStuff() {
+        let content = $("#test").html(renderGames(arr))
+    }
+
+    renderStuff();
 
 })
